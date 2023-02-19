1db4b3fc314a3b86e24c8beec42edb0420d80f50
Stepan Halych <stephengalych@gmail.com>
2023-02-20 00:23:54 +0100

    added logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
                     {!! Menu::Main() !!}
                 </div>
             </nav>
-            <a href="{{route('home', $activeCity['user_location'])}}" class="logo" rel="nofollow">{{setting('site.logo')}}</a>
+            <a href="{{route('home', $activeCity['user_location'])}}" class="logo" rel="nofollow"><img src="{{setting('site.logo')}}" alt="Logo"></a>
             <nav class="navigation navigation-main">
                 {!! Menu::Main() !!}
             </nav>
