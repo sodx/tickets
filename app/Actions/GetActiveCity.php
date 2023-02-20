671dba266ec01ca75eb992fd4cd593a10366bfa7
Stepan Halych <stephengalych@gmail.com>
2023-02-21 00:00:36 +0100

    add user locator

--- app/Actions/GetActiveCity.php
+++ app/Actions/GetActiveCity.php
@@ -3,6 +3,7 @@
 namespace App\Actions;
 
 use Lorisleiva\Actions\Concerns\AsAction;
+use Stevebauman\Location\Facades\Location;
 
 class GetActiveCity
 {
@@ -12,12 +13,61 @@ class GetActiveCity
 
     public $type;
 
+    private $cities;
+
+    private $place;
+
     public function handle()
     {
+        $this->getCities();
+        $this->getLocation();
+        if ($this->isCurrentUserHasCity()) {
+            $this->type = 'city';
+            $this->place = $this->location->cityName;
+        } elseif ($this->isCurrentUserHasState()) {
+            $this->type = 'state';
+            $this->place = $this->location->regionName;
+        } else {
+            $this->type = 'all';
+            $this->place = 'All Cities';
+        }
 
+        ray([
+            'user_location' => request()->cookie('user_location') ?? $this->place,
+            'user_location_type' => request()->cookie('user_location_type') ?? $this->type,
+        ]);
         return [
-            'user_location' => request()->cookie('user_location') ?? 'All Cities',
-            'user_location_type' => request()->cookie('user_location_type') ?? 'city',
+            'user_location' => request()->cookie('user_location')
+                    ?? $this->location->cityName
+                    ?? $this->location->regionName
+                    ?? 'All Cities',
+            'user_location_type' => request()->cookie('user_location_type') ?? $this->type,
         ];
+    }
+
+    private function getLocation()
+    {
+        $ip = '48.188.144.248'; /* Static IP address */
+        $this->location = Location::get($ip);
+    }
+
+    private function getCities()
+    {
+        $this->cities = GetCities::run();
+    }
+
+    private function isCurrentUserHasCity()
+    {
+        foreach ($this->cities as $state => $cities) {
+            if (in_array($this->location->cityName, $cities)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function isCurrentUserHasState()
+    {
+        return in_array($this->location->regionName, $this->cities);
     }
 }
