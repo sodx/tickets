9e15b0a3e32c20e4888b34bad97a479e75c1fecc
Stepan Halych <stephengalych@gmail.com>
2023-02-25 14:42:46 +0100

    Change disclaimer position

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,8 @@
 <footer>
     <div class="container">
-        <p>AFFILIATES DISCLAIMER* The Site contain links to affiliate websites, and we receive an affiliate commission for any purchases made by you on the affiliate website using such links.</p>
         {!! menu('footer') !!}
+        <p>AFFILIATES DISCLAIMER* The Site contain links to affiliate websites, and we receive an affiliate commission for any purchases made by you on the affiliate website using such links.</p>
+
         <div class="copyright">
             <p>{{$_SERVER['SERVER_NAME']}}</p><p><b>{{date("Y")}}</b> All Rights Reserved</p>
         </div>
