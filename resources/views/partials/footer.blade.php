c1982508c2599ecc79b14174f156b2ea9878cc1f
Stepan Halych <stephengalych@gmail.com>
2023-02-25 14:40:13 +0100

    Added disclaimer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,6 @@
 <footer>
     <div class="container">
+        <p>AFFILIATES DISCLAIMER* The Site contain links to affiliate websites, and we receive an affiliate commission for any purchases made by you on the affiliate website using such links.</p>
         {!! menu('footer') !!}
         <div class="copyright">
             <p>{{$_SERVER['SERVER_NAME']}}</p><p><b>{{date("Y")}}</b> All Rights Reserved</p>
