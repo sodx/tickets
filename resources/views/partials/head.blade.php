124004827d7b301538b70e993d5c4577eba3a4f8
Stepan Halych <stephengalych@gmail.com>
2023-04-26 00:05:36 +0200

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,4 +1,6 @@
 <head>
+    <!-- Google tag (gtag.js) -->
+    {!! setting( 'site.gtag' ) !!}
     <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,6 +23,4 @@
         }
     </style>
     <meta name='ir-site-verification-token' value='-483884159'>
-    <!-- Google tag (gtag.js) -->
-    {!! setting( 'site.gtag' ) !!}
 </head>
