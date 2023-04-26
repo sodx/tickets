c4fc32eadef31f631ac417d39e0173625bdf3b7a
Stepan Halych <stephengalych@gmail.com>
2023-04-26 22:55:17 +0200

    Favicon support

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <!-- Google tag (gtag.js) -->
     {!! setting( 'site.gtag' ) !!}
-    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/storage/{{setting('site.favicon')}}" type="image/x-icon">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
